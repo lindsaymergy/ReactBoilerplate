3f0b00c218437acac4a0ccb5380a2a009d31b77d
David Federspiel <dfederspiel@federnet.com>
2017-09-16 22:51:10 -0400

    cleaup: cleaning up and commenting

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,23 +1,32 @@
 // This is an alternative coding style for react which uses object notation. (less than ideal)
 // This approach is being deprecated and should not be used.
 
-var React = require('react');
+import React, {Component} from 'react';
 
-var Avatar = React.createClass({
+class Avatar extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			"alt": props.alt,
+			"user": props.user
+		}
+	}
+
 	render() {
 		return (
 			<img
 				src={this.getPhotoUrl()}
-				alt={'Photo of David'}
+				alt={this.state.alt}
 				width={50}
 				height={50}
-				className="commentPhoto"
+				className="commentPhoto widget"
 			/>
 		);
-	},
-	getPhotoUrl() {
-		return 'https://avatars.githubusercontent.com/dfederspiel?s=50';
 	}
-});
+
+	getPhotoUrl() {
+		return 'https://avatars.githubusercontent.com/' + this.state.user +' ?s=50';
+	}
+};
 
 export default Avatar;
--- src/init.js
+++ src/init.js
@@ -5,13 +5,13 @@ import Greeter from './components/Greeter';
 import NameTag from './components/NameTag';
 import Avatar from './components/Avatar';
 
+
 ReactDOM.render(<Greeter />, document.getElementById('greeter'));
 ReactDOM.render(<NameTag name="Alejandra" />, document.getElementById('tag'));
-ReactDOM.render(<Avatar />, document.getElementById('avatar'));
-
+ReactDOM.render(<Avatar alt="Photo of Matt" user="matthewtruty" />, document.getElementById('avatar'));
 ReactDOM.render(<Greeter />, document.getElementById('greeter1'));
 ReactDOM.render(<NameTag name="David" />, document.getElementById('tag1'));
-ReactDOM.render(<Avatar />, document.getElementById('avatar1'));
+ReactDOM.render(<Avatar alt="Photo of a Coffee Cup" user="dfederspiel"/>, document.getElementById('avatar1'));
 
 // Expose collection to UI
 var Components = require('expose-loader?Components!./components');
