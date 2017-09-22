c964c4082524d324c5e3a15fe8fbe74ab444a40e
David Federspiel <dfederspiel@federnet.com>
2017-09-22 00:22:38 -0400

    fix: removed breaking config entry

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -1,12 +1,4 @@
-//const webpack = require('webpack');
-const path = require('path');
-
 module.exports = {
-    resolve: {
-        modules: [
-            path.resolve('./src')
-        ]
-    },
     entry: [ 'babel-polyfill' ],
     devServer: {
         host: 'localhost',
