3b84f9ff9b1e4cf8ed7c642231cd1cd5149cfa05
David Federspiel <dfederspiel@federnet.com>
2017-10-19 12:59:41 -0400

    component: updated progress bar - ditched animations for the moment

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -9,28 +9,29 @@ class ProgressBar extends Component {
             name: props.name,
             maxValue: props.maxValue,
             value: props.value,
-            currentPercentage: 0,
+            //currentPercentage: 0,
+            //newPercentage: 0,
             color: props.color
         };
-        this.stepper = .001;
+        //this.stepper = .05;
         this.targetPercentage = (this.props.value / this.props.maxValue);
-        setTimeout(this.tick.bind(this), 10);
+        //setTimeout(this.tick.bind(this), 10);
     }
 
-    tick() {
-        this.stepper += .001;
-        let newPercentage = this.state.currentPercentage + this.stepper;
+    // tick() {
+    //     this.stepper += .05;
+    //     this.setState({ newPercentage: this.state.currentPercentage + this.stepper });
 
-        this.setState({
-            currentPercentage: newPercentage > 1 ? 1 : newPercentage
-        });
+    //     this.setState({
+    //         currentPercentage: this.state.newPercentage >= 1 ? 1 : this.state.newPercentage
+    //     });
 
-        if(newPercentage < this.targetPercentage)
-            setTimeout(this.tick.bind(this), 10);
-    }
+    //     if(this.state.newPercentage < this.targetPercentage)
+    //         setTimeout(this.tick.bind(this), 10);
+    // }
 
     getRadius() {
-        return '15px ' + ((this.state.currentPercentage >= 1) ? '15px 15px' : '0 0') + ' 15px';
+        return '15px ' + ((this.targetPercentage * 100 >= 100) ? '15px 15px' : '0 0') + ' 15px';
     }
 
     render() {
@@ -38,8 +39,8 @@ class ProgressBar extends Component {
             <div className="progress-bar-wrapper">
                 <label>Progress &reg; Bar</label>
                 <div className="progress-bar">
-                    <div className="progress-bar-progress" style={{ backgroundColor: this.props.color, width: this.state.currentPercentage * 100 + '%', borderRadius: this.getRadius() }}>
-                        <div className="percentage">{Math.round(this.state.currentPercentage * 100)}</div>
+                    <div className="progress-bar-progress" style={{ transition: 'width 15s' , backgroundColor: this.props.color, maxWidth: this.targetPercentage * 100 + '%', borderRadius: this.getRadius() }}>
+                        <div className="percentage">{Math.round(this.targetPercentage * 100)}</div>
                     </div>
                 </div>
             </div>
--- src/components/ProgressBar/__tests__/ProgressBar.test.js
+++ src/components/ProgressBar/__tests__/ProgressBar.test.js
@@ -15,14 +15,26 @@ describe('the progress bar component', () => {
         expect(tree).toMatchSnapshot();
     });
 
-    it('will increase the width of the progress for each tick', () => {
-        var ProgressBarComponent = ReactTestUtils.renderIntoDocument(<ProgressBar maxValue={100} value={100} />);
-        ProgressBarComponent.tick();
+    // it('will change the width of the control with each tick', () => {
+    //     var ProgressBarComponent = ReactTestUtils.renderIntoDocument(<ProgressBar maxValue={100} value={10} />);
+    //     ProgressBarComponent.tick();
+    //     ProgressBarComponent.state.newPercentage = 1;
+    //     ProgressBarComponent.tick();
+    //     ProgressBarComponent.state.newPercentage = -1;
+    //     ProgressBarComponent.tick();
+    //     ProgressBarComponent.state.newPercentage = 10;
+    //     ProgressBarComponent.tick();
+    // });
+
+    it('will set a radius of 15px for just the TL & BL of the bar if current percentage is less than 1', () => {
+        var ProgressBarComponent = ReactTestUtils.renderIntoDocument(<ProgressBar maxValue={100} value={50} />);
         var radius = ProgressBarComponent.getRadius();
         expect(radius).toBe('15px 0 0 15px');
+    });
 
-        jest.useFakeTimers();
-        jest.runOnlyPendingTimers();
-
+    it('will set a radius of 15px for all corners of the bar if current percentage is equal to or greater than 1', () => {
+        var ProgressBarComponent = ReactTestUtils.renderIntoDocument(<ProgressBar maxValue={100} value={100} />);
+        var radius = ProgressBarComponent.getRadius();
+        expect(radius).toBe('15px 15px 15px 15px');
     });
 });
