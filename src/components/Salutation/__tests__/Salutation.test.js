931350674e8b06634e5ee0d753c469d50cb214fe
David Federspiel <dfederspiel@federnet.com>
2017-10-23 11:43:52 -0400

    fix: SASS loader

---
+++ src/components/Salutation/__tests__/Salutation.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactTestUtils from 'react-dom/test-utils';
+import renderer from 'react-test-renderer';
+
+import Salutation from '../Salutation';
+
+describe('the hello world component', () => {
+    it('renders correctly', () => {
+        const tree = renderer.create(
+            <Salutation />
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    describe('given no words', () => {
+        it('displays a default salutation', () => {
+            let SalutationComponent = ReactTestUtils.renderIntoDocument(<Salutation />);
+            let h1 = ReactTestUtils.findRenderedDOMComponentWithTag(SalutationComponent, 'h1');
+            expect(h1.textContent).toBe('Salutations!');
+        });
+    });
+
+    describe('given a word', ()=> {
+        it('displays that word instead of the default', () => {
+            let SalutationComponent = ReactTestUtils.renderIntoDocument(<Salutation words="Hello!" />);
+            let h1 = ReactTestUtils.findRenderedDOMComponentWithTag(SalutationComponent, 'h1');
+            expect(h1.textContent).toBe('Hello!');
+        });
+    });
+
+    describe('given a comma separated list of values', () => {
+        it('displays one word from the list', () => {
+            let SalutationComponent = ReactTestUtils.renderIntoDocument(<Salutation words="Hello!, Hello!" />);
+            let h1 = ReactTestUtils.findRenderedDOMComponentWithTag(SalutationComponent, 'h1');
+            expect(h1.textContent).toBe('Hello!');
+        });
+    });
+});
